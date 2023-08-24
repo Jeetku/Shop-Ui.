50721688d6cc2451d4d7af15fa7856a164a170f5
Jeetesh <jeeteshkumar45@gmail.com>
2023-08-24 14:16:44 +0530

    Announcements

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Announcemant from "../components/Announcemant";
 
 const Home = () => {
   return (
     <div>
+      <Announcemant />
       <Navbar />
     </div>
   );
