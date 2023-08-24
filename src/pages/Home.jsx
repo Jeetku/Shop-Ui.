344d9e0f3324ce3a74cf7918dd26736d41def87e
Jeetesh <jeeteshkumar45@gmail.com>
2023-08-24 15:56:31 +0530

    Slider section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Announcemant from "../components/Announcemant";
+import Slider from "../components/Slider";
 
 const Home = () => {
   return (
     <div>
       <Announcemant />
       <Navbar />
+      <Slider />
     </div>
   );
 };
